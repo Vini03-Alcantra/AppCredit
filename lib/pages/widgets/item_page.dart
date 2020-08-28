df41f1a5845b90724177ceb2aaf00a08c1c1a633
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-08-28 12:15:05 -0300

    alterações na itempage.dart

--- lib/pages/widgets/item_page.dart
+++ lib/pages/widgets/item_page.dart
@@ -1,5 +1,6 @@
+import 'package:creditcard/controllers/offset_controller.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 class ItemPage extends StatelessWidget {
   final Color color;
   final int index;
@@ -11,35 +12,49 @@ class ItemPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children:<Widget>[
-      Positioned(        
-        top: MediaQuery.of(context).size.height / 3,
-        height: MediaQuery.of(context).size.height / 1.6,
-        width: MediaQuery.of(context).size.width / 1.25,
-        child: Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: RotatedBox(
-              quarterTurns: 1,
-              child: Image.network(imageURL, fit: BoxFit.cover)
+    return Consumer<PageController>(      
+      child: Container(
+        child:ClipRRect(  
+          borderRadius: BorderRadius.circular(20),
+          child: RotatedBox(
+            quarterTurns: 1,  
+            child: Image.network(  
+              imageURL,
+              fit: BoxFit.cover
             )
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                offset: Offset(0, 10),
-                blurRadius: 15
-              )
-            ],
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.blue
-          ),
+          )
         ),
+        margin: EdgeInsets.symmetric(  
+          horizontal: 20,
+          vertical: 20
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 10),
+              blurRadius: 15
+            ),            
+          ],
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.blue
+        )
       ),
-      ]
+      builder: (BuildContext context, PageController value, Widget child) { 
+        return Stack(
+          children: <Widget>[
+            Positioned(  
+              top: MediaQuery.of(context).size.height / 3,
+              height: MediaQuery.of(context).size.height / 1.8,
+              width: MediaQuery.of(context).size.width - 90,
+              child: Transform.rotate(
+                angle: 0,
+                child: child,
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
