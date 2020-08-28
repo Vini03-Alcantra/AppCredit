5b50408b578848e205a8cd49270de8dfdefbe37b
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-08-28 11:40:24 -0300

    adicionando changenotifierprovider como claase mãe

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:creditcard/controllers/offset_controller.dart';
 import 'package:creditcard/pages/widgets/item_page.dart';
 import 'package:creditcard/pages/widgets/my_app_bar.dart';
 import 'package:creditcard/pages/widgets/painel_top.dart';
@@ -14,24 +15,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            MyAppBar(),
-            PainelTop(),
-            PageView(
-              onPageChanged: (index){
-                Provider.of<PageController>(context, listen: false).jumpToPage(index);
-              },
-              controller: _pageController,
-              children: <Widget>[
-                ItemPage(imageURL: "https://us.123rf.com/450wm/grebeshkovmaxim/grebeshkovmaxim1805/grebeshkovmaxim180500202/100769321-colorful-smooth-gradient-color-background-design-for-your-project-design-.jpg?ver=6",),
-                ItemPage(imageURL: "https://ak5.picdn.net/shutterstock/videos/1018020805/thumb/1.jpg"),
-                ItemPage(imageURL: "https://i.pinimg.com/736x/5c/ee/af/5ceeafdc811d2269821fb03df29374ff.jpg",),
-              ],
-            )
-          ],
-        )
+      body: ChangeNotifierProvider(
+        create: (BuildContext context) => OffsetController(_pageController),
+        child: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              MyAppBar(),
+              PainelTop(),
+              PageView(
+                onPageChanged: (index){
+                  Provider.of<PageController>(context, listen: false).jumpToPage(index);
+                },
+                controller: _pageController,
+                children: <Widget>[
+                  ItemPage(imageURL: "https://us.123rf.com/450wm/grebeshkovmaxim/grebeshkovmaxim1805/grebeshkovmaxim180500202/100769321-colorful-smooth-gradient-color-background-design-for-your-project-design-.jpg?ver=6",),
+                  ItemPage(imageURL: "https://ak5.picdn.net/shutterstock/videos/1018020805/thumb/1.jpg"),
+                  ItemPage(imageURL: "https://i.pinimg.com/736x/5c/ee/af/5ceeafdc811d2269821fb03df29374ff.jpg",),
+                ],
+              )
+            ],
+          )
+        ),
       ),
     );
   }
