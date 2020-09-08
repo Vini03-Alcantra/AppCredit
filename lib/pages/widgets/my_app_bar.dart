a1c227d1cb3d32a6ecc5e2594764b45b70d93651
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-08 15:54:46 -0300

    Botão back sendo exibido apenas quando necessário

--- lib/pages/widgets/my_app_bar.dart
+++ lib/pages/widgets/my_app_bar.dart
@@ -5,22 +5,33 @@ import 'package:provider/provider.dart';
 class MyAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(      
-      height: MediaQuery.of(context).size.height * 0.08,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      //padding: EdgeInsets.only(top: (MediaQuery.of(context).size.width * 0.05) + MediaQuery.of(context).padding.top, left: MediaQuery.of(context).size.width * 0.05, right: MediaQuery.of(context).size.width * 0.05),
+      //padding: const EdgeInsets.all(8.0),
+      child: SizedBox(            
+        height: MediaQuery.of(context).size.height * 0.08,
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            IconButton(
-              splashColor: Colors.black12,                              
-              icon: Icon(Icons.arrow_back, size: 28),
-              onPressed: (){                
-                Provider.of<PageControllerApp>(context, listen: false)
-                  .setCurrentIndex(-1);
+            Consumer<PageControllerApp>( 
+              builder: (context, value, child){
+                return AnimatedOpacity(
+                  opacity: value.currentIndex != -1 ? 1 : 0,
+                  duration: Duration(milliseconds: 300),
+                  child:child                
+                );              
               },
-            ),            
+              child: IconButton(
+                splashColor: Colors.black12,                              
+                icon: Icon(Icons.arrow_back, size: 28),
+                onPressed: (){                
+                  Provider.of<PageControllerApp>(context, listen: false)
+                    .setCurrentIndex(-1);
+                  },
+                ),
+            ),                        
             Consumer<PageControllerApp>(
               builder: (context, value, child){
                 return AnimatedOpacity(
